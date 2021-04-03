986e62cff004bdcb6dc7f09de0daac6870a1fe0c
Gleb Stachev <stachev.gleb@gmail.com>
2021-04-03 19:46:55 +0300

    update(js, css, html):displayFunc

--- index.js
+++ index.js
@@ -8,46 +8,43 @@ createBtn();
 
 function createBtn(){
 for (let i = 0; i < 9; i++){
-    btnWrap.innerHTML += `<button id="${i+1}" class="button${i+1} button"><p class="pNum">${i+1}</p></button>`
+    btnWrap.innerHTML += `<button dataBtn="${i+1}" id="${i+1}" class="button${i+1} button"><p class="pNum">${i+1}</p></button>`
 }
-btnWrap.innerHTML += `<button class="button">0</button>`;
-btnWrap.innerHTML += `<button class="button">.</button>`;
-btnWrap.innerHTML += `<button class="buttonEqual">=</button>`;
-topBtn.innerHTML += `<button class="operBtnStyle ce">CE</button>`;
-topBtn.innerHTML += `<button class="operBtnStyle addmenu" id="addMenu">...</button>`;
-topBtn.innerHTML += `<button class="operBtnStyle additionalSings">(</button>`;
-topBtn.innerHTML += `<button class="operBtnStyle additionalSings">)</button>`;
-bottomBtn.innerHTML += `<button class="operBtnStyle mainSings" id="minus">-</button>`;
-bottomBtn.innerHTML += `<button class="operBtnStyle mainSings" id="plus">+</button>`;
-bottomBtn.innerHTML += `<button class="operBtnStyle mainSings" id="division">÷</button>`;
-bottomBtn.innerHTML += `<button class="operBtnStyle mainSings" id="multiplication">×</button>`;
+btnWrap.innerHTML += `<button dataBtn="0" class="button">0</button>`;
+btnWrap.innerHTML += `<button dataBtn="." class="button">.</button>`;
+btnWrap.innerHTML += `<button dataBtn="=" class="buttonEqual">=</button>`;
+topBtn.innerHTML += `<button dataBtn1="CE" class="operBtnStyle ce">CE</button>`;
+topBtn.innerHTML += `<button dataBtn="..." class="operBtnStyle addmenu button" id="addMenu">...</button>`;
+topBtn.innerHTML += `<button dataBtn="(" class="operBtnStyle additionalSings button">(</button>`;
+topBtn.innerHTML += `<button dataBtn=")" class="operBtnStyle additionalSings button">)</button>`;
+bottomBtn.innerHTML += `<button dataBtn="-" class="operBtnStyle mainSings button" id="minus">-</button>`;
+bottomBtn.innerHTML += `<button dataBtn="+" class="operBtnStyle mainSings button" id="plus">+</button>`;
+bottomBtn.innerHTML += `<button dataBtn="÷" class="operBtnStyle mainSings button" id="division">÷</button>`;
+bottomBtn.innerHTML += `<button dataBtn="×" class="operBtnStyle mainSings button" id="multiplication">×</button>`;
 
-changeableBtn.innerHTML += `<button class="operBtnStyle addOperBtnStyle mainSings">%</button>`;
-changeableBtn.innerHTML += `<button class="operBtnStyle addOperBtnStyle mainSings">√</button>`;
-changeableBtn.innerHTML += `<button class="operBtnStyle addOperBtnStyle mainSings">²</button>`;
-changeableBtn.innerHTML += `<button class="operBtnStyle addOperBtnStyle mainSings">π</button>`;
+changeableBtn.innerHTML += `<button dataBtn="%" class="operBtnStyle addOperBtnStyle mainSings button">%</button>`;
+changeableBtn.innerHTML += `<button dataBtn="√" class="operBtnStyle addOperBtnStyle mainSings button">√</button>`;
+changeableBtn.innerHTML += `<button dataBtn="²" class="operBtnStyle addOperBtnStyle mainSings button">²</button>`;
+changeableBtn.innerHTML += `<button dataBtn="π" class="operBtnStyle addOperBtnStyle mainSings button">π</button>`;
 }
 
 let addMenu = document.getElementById('addMenu');
-
-function addMenuFunc() {
-    changeableBtnFunc() 
-}
 
 function changeableBtnFunc() {
     bottomBtn.classList.toggle('hide');
     changeableBtn.classList.toggle('hide');
 }
 
-addMenu.addEventListener('click', addMenuFunc);
+addMenu.addEventListener('click', changeableBtnFunc);
 
-// function displayedValue (){
-//     let displayNum = this.getAttribute('id');
-//     display.innerText += displayNum;
-//     console.log(displayNum, display);
-// }
-//  let btnValue = document.querySelector('.button');
- 
-//  btnValue.forEach((item, i) => {
-//      item.addEventListner('click', displayedValue)
-//     });
+function displayValue (){
+    let displayNum = this.getAttribute('dataBtn');
+    display.innerText += displayNum;
+    // console.log(displayNum, display);
+}
+ let btnValue = document.querySelectorAll('.button');
+//  console.log(btnValue);
+
+ btnValue.forEach(function(item){
+     return item.addEventListner('click', displayValue)
+    });
